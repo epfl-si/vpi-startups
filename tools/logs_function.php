0546f898e2570093d71d0a7331e915d1c19d937d
strikerpt <hugo.valente1996@gmail.com>
2021-05-26 09:49:49 +0200

    logs function to write them in the database

--- tools/logs_function.php
+++ tools/logs_function.php
@@ -6,7 +6,7 @@ function add_logs($sciper_number,$before,$after,$action)
     require 'connection_db.php';
     $date = date("Y-m-d H:i:s");
 
-    $insert_logs_data = $db -> prepare('INSERT INTO logs(sciper_number,date_logs,after_changes,before_changes,action) VALUES ("'.$sciper_number.'","'.$date.'","'.$after.'","'.$before.'","'.$action.'")');
+    $insert_logs_data = $db -> prepare('INSERT INTO logs(sciper_number,date_logs,before_changes,after_changes,action) VALUES ("'.$sciper_number.'","'.$date.'","'.$before.'","'.$after.'","'.$action.'")');
     $insert_logs_data -> execute();
 }
 
