f0f678501e4aff97e57b6e1621597b47924f5f9e
strikerpt <hugo.valente1996@gmail.com>
2021-05-26 09:40:07 +0200

    add the fields to automatically fill them with the data of the sciper number

--- add_new_person.php
+++ add_new_person.php
@@ -19,7 +19,35 @@
                     <div class="form-group row">
                         <label for="sciper_number" class="col-sm-4 col-form-label">Sciper Number <small class="text-danger"> *</small> </label>
                         <div class="col-sm-6">
-                            <input type="number" min="100000" max="999999" class="form-control" name="sciper_number" id="sciper_number" required>
+                            <input type="number" min="100000" max="999999" class="form-control" name="sciper_number" onchange="get_data()" id="sciper_number" required>
+                        </div>
+                    </div>
+                    <!-- Champ pour ajouter une personne lier à un numero de sciper -->
+                    <div class="form-group row">
+                        <label for="name" class="col-sm-4 col-form-label">Name <small class="text-danger"> *</small> </label>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" name="name" id="name" required>
+                        </div>
+                    </div>
+                    <!-- Champ pour ajouter une personne lier à un numero de sciper -->
+                    <div class="form-group row">
+                        <label for="firstname" class="col-sm-4 col-form-label">Firstname <small class="text-danger"> *</small> </label>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" name="firstname" id="firstname" required>
+                        </div>
+                    </div>
+                    <!-- Champ pour ajouter une personne lier à un numero de sciper -->
+                    <div class="form-group row">
+                        <label for="person_function" class="col-sm-4 col-form-label">Person function <small class="text-danger"> *</small> </label>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" name="person_function" id="person_function" required>
+                        </div>
+                    </div>
+                    <!-- Champ pour ajouter une personne lier à un numero de sciper -->
+                    <div class="form-group row">
+                        <label for="email_person" class="col-sm-4 col-form-label">Email <small class="text-danger"> *</small> </label>
+                        <div class="col-sm-6">
+                            <input type="email" class="form-control" name="email_person" id="email_person" required>
                         </div>
                     </div>
                     <!-- Champ pour choisir si la personne est professeur -->
@@ -44,32 +72,53 @@
                         </select>
                         </div>
                     </div>
-                    <!-- Champ pour choisir une startup -->
-                    <div class="form-group row">
-                        <label for="startup" class="col-sm-4 col-form-label">Type of Person <small class="text-danger"> *</small> </label>
-                        <div class="col-sm-6">
-                        <select class="form-control" class="selectpicker" data-dropup-auto="true" name="type_of_person" id="type_of_person" required>
-                            <option name="NULL" value="" disabled selected>Select a type of person</option>';
-                            $type_of_person_data = $db-> query('SELECT type_of_person FROM type_of_person');
-                            $data_type_of_person = $type_of_person_data -> fetchAll();
-                            foreach ($data_type_of_person as $type_of_person)
-                            {
-                                echo '<option value="'.$type_of_person['type_of_person'].'">'.$type_of_person['type_of_person'].'</option>';
-                            }
-                        echo '
-                        </select>
-                        </div>
-                    </div>
                     <button class="btn btn-outline-secondary mt-5" id="submit_new_person" name="submit_new_person" type="submit">Submit</button>
                 </form>
-            </div>';
+            </div>
+            
+            <script>
 
+            function get_data()
+            {
+                var sciper = document.getElementById("sciper_number").value;
+                console.log(sciper);
+                $.ajax
+                ({  
+                    //Chemin vers la page qui contient les requêtes SQL
+                    url:"tools/get_data_from_ldap.php",
+                    method:"POST",
+                    dataType:"JSON",
+                    data: 
+                    {
+                        sciper : sciper,
+                    },
+                    /*Si tout est bien, il affiche un pop-up, en disant que les changements
+                    ont été faits et il rafraîchit la page pour montrer à l\"utilisateur les changements*/
+                    success:function(data)
+                    {
+                        document.getElementById("name").value = data[0]["name"];
+                        document.getElementById("firstname").value = data[0]["firstname"];
+                        document.getElementById("email_person").value = data[0]["email"];
+                        document.getElementById("person_function").value = data[0]["person_function"];
+                    },
+                    error:function()
+                    {
+                        alert("Something went wrong, please try again.");
+                    }
+                });
+            }
+
+            </script>';
+                
             if(isset($_POST['submit_new_person']))
             {
                 $sciper = (int)$_POST['sciper_number'];
+                $name = security_text($_POST['name']);
+                $firstname = security_text($_POST['firstname']);
+                $email = security_text($_POST['email']);
+                $person_function = security_text($_POST['person_function']);
                 $prof_as_founder = $_POST['prof_as_founder'];
                 $gender = $_POST['gender'];
-                $type_of_person = $_POST['type_of_person'];
 
                 // LDAP variables
                 $ldapuri = "ldap://ldap.epfl.ch:389";
@@ -87,18 +136,12 @@
                 $info = ldap_get_entries($ldapconn, $sr);
 
                 if($info["count"])
-                {
-                    $email = $info[0]["mail"][0];
-                    $firstname = $info[0]["givenname"][0];
-                    $name = $info[0]["sn"][0];
-                    $person_function = $info[0]["title"][0];
-                    
+                {                    
                     $persons = $db ->query('SELECT sciper_number FROM person WHERE sciper_number = "'.$sciper.'"');
                     $person = $persons -> fetch();
 
                     if($person == "")
                     {
-                   
                         $add_new_person = $db ->prepare('INSERT INTO person(name,firstname,person_function,email,prof_as_founder,gender,sciper_number) VALUES("'.$name.'","'.$firstname.'","'.$person_function.'","'.$email.'","'.$prof_as_founder.'","'.$gender.'","'.$sciper.'")');
                         $add_new_person -> execute();
 
