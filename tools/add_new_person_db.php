6faaa9083a96e8a2a6a991d7a8ec27d12d831899
strikerpt <hugo.valente1996@gmail.com>
2021-06-18 14:10:15 +0200

    erase unncessary ldap query

--- tools/add_new_person_db.php
+++ tools/add_new_person_db.php
@@ -9,65 +9,35 @@ if(isset($_POST['submit_new_person']))
     $person_function = security_text($_POST['person_function']);
     $prof_as_founder = $_POST['prof_as_founder'];
     $gender = $_POST['gender'];
+                    
+    $persons = $db ->query('SELECT sciper_number FROM person WHERE sciper_number = "'.$sciper.'"');
+    $person = $persons -> fetch();
 
-    // LDAP variables
-    $ldapuri = "ldap://ldap.epfl.ch:389";
-
-    // Connecting to LDAP
-    $ldapconn = ldap_connect($ldapuri)
-            or die("That LDAP-URI was not parseable");
-
-    $dn = "o=epfl, c=CH";
-    $filter="(uniqueIdentifier=$sciper)";
-    $justthese = array("ou", "sn", "givenname", "mail", "title");
-
-    $sr=ldap_search($ldapconn, $dn, $filter, $justthese);
-
-    $info = ldap_get_entries($ldapconn, $sr);
-
-    if($info["count"])
-    {                    
-        $persons = $db ->query('SELECT sciper_number FROM person WHERE sciper_number = "'.$sciper.'"');
-        $person = $persons -> fetch();
-
-        if($person == "")
-        {
-            $add_new_person = $db ->prepare('INSERT INTO person(name,firstname,person_function,email,prof_as_founder,gender,sciper_number) VALUES("'.$name.'","'.$firstname.'","'.$person_function.'","'.$email.'","'.$prof_as_founder.'","'.$gender.'","'.$sciper.'")');
-            $add_new_person -> execute();
-
-            //Ecrire les données dans la table logs pour dire que l'utilisateur à fait un export
-            $before = "";
-            $after = "Name : ".$name." Fistname : ".$firstname." Person Function : ".$person_function." Email : ".$email." Prof as founder : ".$prof_as_founder." Gender : ".$gender." sciper_number : ".$sciper;
-            $action="Add new person";
-
-            add_logs($_SESSION['uniqueid'],$before,$after,$action);
-
-            $_SESSION['flash_message'] = array();
-            $_SESSION['flash_message']['message'] = "The person ".$firstname." ".$name." was added in database";
-            $_SESSION['flash_message']['type'] = "success";
-            header("Location: /$controller/$method");
-        }
-        else
-        {   
-            $_SESSION['flash_message'] = array();
-            $_SESSION['flash_message']['message'] = "The person already exist in database";
-            $_SESSION['flash_message']['type'] = "warning";
-            header("Location: /$controller/$method");
-            
-        }
-
-    }
-    else
+    if($person == "")
     {
+        $add_new_person = $db ->prepare('INSERT INTO person(name,firstname,person_function,email,prof_as_founder,gender,sciper_number) VALUES("'.$name.'","'.$firstname.'","'.$person_function.'","'.$email.'","'.$prof_as_founder.'","'.$gender.'","'.$sciper.'")');
+        $add_new_person -> execute();
+
+        //Ecrire les données dans la table logs pour dire que l'utilisateur à fait un export
+        $before = "";
+        $after = "Name : ".$name." Fistname : ".$firstname." Person Function : ".$person_function." Email : ".$email." Prof as founder : ".$prof_as_founder." Gender : ".$gender." sciper_number : ".$sciper;
+        $action="Add new person";
+
+        add_logs($_SESSION['uniqueid'],$before,$after,$action);
 
         $_SESSION['flash_message'] = array();
-        $_SESSION['flash_message']['message'] = "Sciper number not found";
-        $_SESSION['flash_message']['type'] = "warning";
+        $_SESSION['flash_message']['message'] = "The person ".$firstname." ".$name." was added in database";
+        $_SESSION['flash_message']['type'] = "success";
         header("Location: /$controller/$method");
     }
-
-
-    
+    else
+    {   
+        $_SESSION['flash_message'] = array();
+        $_SESSION['flash_message']['message'] = "The person already exist in database";
+        $_SESSION['flash_message']['type'] = "warning";
+        header("Location: /$controller/$method");
+        
+    }    
 }
 
 
