683e47a7ff414bf5bec5be2371a0a9b2506d00da
strikerpt <hugo.valente1996@gmail.com>
2021-06-21 18:16:08 +0200

    add condition to get funds by startup or all the funds and to use SQL view

--- tools/funds_db.php
+++ tools/funds_db.php
@@ -2,20 +2,22 @@
 
 require 'connection_db.php';
 
+$id_startup = $_POST['id_startup'];
+
+if($id_startup=="none")
+{
+    $query = 'SELECT * FROM view_display_funds';
+}
+else
+{
+    $query = 'SELECT * FROM view_display_funds WHERE fk_startup = "'.$id_startup.'"';
+}
+
 //Chercher les données du nombre de startups par secteur avec une vue SQL
-$funds_db= $db ->query('SELECT * FROM funding');
+$funds_db= $db ->query($query);
 $funds = $funds_db ->fetchAll();
 foreach ($funds as $fund)
 {
-    $stages_of_investment_db= $db ->query('SELECT * FROM stage_of_investment WHERE id_stage_of_investment = "'.$fund['fk_stage_of_investment'].'"');
-    $stage = $stages_of_investment_db ->fetch();
-
-    $types_of_investment_db= $db ->query('SELECT * FROM type_of_investment WHERE id_type_of_investment = "'.$fund['fk_type_of_investment'].'"');
-    $type = $types_of_investment_db ->fetch();
-
-    $startups_db= $db ->query('SELECT * FROM startup WHERE id_startup = "'.$fund['fk_startup'].'"');
-    $startup = $startups_db ->fetch();
-
     //Faire un tableau avec les données nécessaires pour le graphique
     $output[] = array 
     (
@@ -23,9 +25,9 @@ foreach ($funds as $fund)
         'amount'=> $fund['amount'],
         'investment_date' => $fund['investment_date'],
         'investors' => $fund['investors'],
-        'stage_of_investment' => $stage['stage_of_investment'],
-        'type_of_investment' => $type['type_of_investment'],
-        'startup' => $startup['company'],
+        'stage_of_investment' => $fund['stage_of_investment'],
+        'type_of_investment' => $fund['type_of_investment'],
+        'startup' => $fund['company'],
     );
 
 }
