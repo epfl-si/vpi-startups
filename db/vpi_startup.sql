8b83d3a93728c191391ed82a81b83f734182f3bb
strikerpt <hugo.valente1996@gmail.com>
2021-06-21 09:31:48 +0200

    Update structure of database

--- db/vpi_startup.sql
+++ db/vpi_startup.sql
@@ -866,4 +866,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-18 14:56:26
+-- Dump completed on 2021-06-21  7:31:48
