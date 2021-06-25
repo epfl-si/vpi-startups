25f8b6455fe2099bb44be37179fdcd47459bf0f0
strikerpt <hugo.valente1996@gmail.com>
2021-06-25 13:45:30 +0200

    Update structure of database

--- db/vpi_startup_only_structure.sql
+++ db/vpi_startup_only_structure.sql
@@ -914,4 +914,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-23 13:14:14
+-- Dump completed on 2021-06-25 11:45:30
