0c21e914eada77cfe94b4cebbea9a9b7eb023ead
strikerpt <hugo.valente1996@gmail.com>
2021-07-05 15:07:56 +0200

    add method to get POST value and put it in the db query

---
+++ tools/intermediate_db.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'connection_db.php';
+
+$controller = $_POST['controller'];
+
+//Chercher les données dans la base de données
+$intermediate_db= $db ->query("SELECT * FROM $controller");
+$intermediates = $intermediate_db ->fetchAll();
+foreach ($intermediates as $intermediate)
+{
+    //Faire un tableau avec les données nécessaires pour le graphique
+    $output[] = array 
+    (
+        "id_$controller" => $intermediate["id_$controller"],
+        "$controller"=> $intermediate["$controller"],
+    );
+
+}
+//Enovyer les données de l'array
+echo json_encode($output);
+
+?>
