2994e3d88020eddd11044865ac843276d8201ab9
strikerpt <hugo.valente1996@gmail.com>
2021-07-05 15:07:12 +0200

    correct page to be more generic and get data from all intermediate tables without changing the code

--- pages/intermediate_tables/intermediate_data.php
+++ pages/intermediate_tables/intermediate_data.php
@@ -1,37 +1,36 @@
 <?php
 
 //Importer les fichiers
-require './classes/class.type_of_startup.php';
+require './classes/class.intermediate_data.php';
 
 //Ajouter une nouvelle donnée
 if($method=="add")
 {
-    //Ajouter un nouveau type de startup
-    if($controller == "type_startup")
-    {
+    
         //Faire appel à la classe fund
-        $type_startup = new Type_of_startup();
+        $intermediate_data = new Intermediate_data();
 
-        //Donner un titre au formulaire qui correspond 
+        //Donner un titre au formulaire qui correspond
         $option_type_of_form = "Add New ";
-        $option_title = "Type of Startup";
-        $option_name_post = "add_new_type_of_startup";
+        $option_title = str_replace ( "_", " ", $controller);
+
+        $option_name_post = "add_new_".$controller;
 
         //Si le bouton submit a été cliqué
         if(isset($_POST['submit_btn_intermediate']))
         {
             //Il fait appel à la fonction pour insérer les nouvelles données. En paramètre, il a tout les données du formulaire passées en POST
-            if($type_startup->insert_new_type_of_startup_data($_POST))
+            if($intermediate_data->insert_new_data($_POST, $controller))
             {
                 //Texte pour le log
-                $after = "type_startup : ".$_POST['add_new_type_of_startup'];
-                $action = "Add new type of startup";
+                $after = "$controller : ".$_POST["add_new_$controller"];
+                $action = "Add new $controller";
 
                 //Additionner le log à la base de données (le before est vide car pour un ajout, il n'y a pas d'avant)
                 add_logs($_SESSION['uniqueid'],"",$after,$action);
 
                 //Flash message pour dire que le fond a été ajouté à la base de données
-                $_SESSION['flash_message']['message'] = "The type of startup was added";
+                $_SESSION['flash_message']['message'] = "The $controller was added";
                 $_SESSION['flash_message']['type'] = "success";
 
                 //Rediriger l'utilisateur vers la même page pour qu'il puisse voir le flash message
@@ -43,58 +42,51 @@ if($method=="add")
 
         //Importer le formulaire
         require_once("./pages/intermediate_tables/form_intermediate_data.html");
-    }
+    
 }
 //Modifier un fond si la méthode est égale à modify
 elseif($method=="modify" && is_numeric($param) && !empty($param))
 {
-    if($controller == "type_startup")
+    
+    //Faire appel à la classe fund
+    $intermediate_data = new Intermediate_data();
+
+    //Donner un titre au formulaire qui correspond 
+    $option_type_of_form = "Modify ";
+    $option_title = str_replace ( "_", " ", $controller);
+    $option_name_post = "modify_$controller";
+
+    //Récupérer la valeur qui correspond à l'id qui est un paramètre
+    $option_data = $intermediate_data->get_data_by_id($param, $controller);
+
+    //Mettre dans une variable session la valeur déjà existante dans la db pour l'id en paramètre
+    $get_data_to_modify = $intermediate_data->get_data_to_modify($param, $controller);
+
+    if(isset($_POST['submit_btn_intermediate']))
     {
-        //Faire appel à la classe fund
-        $type_startup = new Type_of_startup();
-
-        //Donner un titre au formulaire qui correspond 
-        $option_type_of_form = "Modify ";
-        $option_title = "Type of Startup";
-        $option_name_post = "modify_type_of_startup";
-
-        //Récupérer la valeur qui correspond à l'id qui est un paramètre
-        $option_data = $type_startup->get_type_of_startup_by_id($param);
-
-        //Mettre dans une variable session la valeur déjà existante dans la db pour l'id en paramètre
-        $get_type_of_startup_to_modify = $type_startup->get_type_of_startup_to_modify($param);
-
-        if(isset($_POST['submit_btn_intermediate']))
+        //Cette condition permet de vérifier si un champ a été changé
+        if(intermediate_data_has_been_modify($param, $controller))
         {
-            //Cette condition permet de vérifier si un champ a été changé
-            if(type_startup_data_has_been_modify($param))
-            {
-                //S'il y a un changement, il fait l'update
-                if($type_startup->update_type_of_startup_data($_POST, $param))
-                {   
-                    //Texte des logs
-                    $before = "type_startup : ".$get_type_of_startup_to_modify['type_startup'];
-                    $after = "type_startup : ".$_POST['modify_type_of_startup'];
-                    $action = "Modify type startup";
-                    
-                    //Ajouter les logs avec l'avant et l'après
-                    add_logs($_SESSION['uniqueid'],$before,$after,$action);
+            //S'il y a un changement, il fait l'update
+            if($intermediate_data->update_data($_POST, $param, $controller))
+            {   
+                //Texte des logs
+                $before = "$controller : ".$get_data_to_modify["$controller"];
+                $after = "$controller : ".$_POST["modify_$controller"];
+                $action = "Modify $controller";
+                
+                //Ajouter les logs avec l'avant et l'après
+                add_logs($_SESSION['uniqueid'],$before,$after,$action);
 
-                    $_SESSION['flash_message']['message'] = "The type of startup was changed";
-                    $_SESSION['flash_message']['type'] = "success";
-                    //header("Location: /type_startup/modify/$param");
-                }
+                $_SESSION['flash_message']['message'] = "The $controller was changed";
+                $_SESSION['flash_message']['type'] = "success";
+                //header("Location: /type_startup/modify/$param");
             }
         }
-
-
-
-
-
-
-        //Importer le formulaire
-        require_once("./pages/intermediate_tables/form_intermediate_data.html");
     }
+
+    //Importer le formulaire
+    require_once("./pages/intermediate_tables/form_intermediate_data.html");
 }
 
 //S'il n'y pas de methode, alors il affiche le tableau avec tous les fonds de toutes les startups
@@ -102,35 +94,12 @@ elseif($method=="modify")
 {
     require 'pages/intermediate_tables/intermediate_display_table.php';
 
-    if($controller == "type_startup")
-    {
-        $filename_queries_db = "type_startup_db.php";
-        $type_data = "type_startup";
+    
+    $filename_queries_db = "intermediate_db.php";
+    $type_data = $controller;
 
-        intermediate_table($filename_queries_db, $type_data);
-    }
+    intermediate_table($filename_queries_db, $type_data);
+    
 
-    /*
-    Condition qui permet de vérifier si l'utisateur a cliquer sur le bouton d'exporter les fonds. 
-    Si la variable est initialisée, il n'importe pas le tableau et permet de télécharger seulement les données de la base de données et va supprimer la variable pour permettre aux utilisateur de voir le tableau des fonds.
-    Si la variable n'est pas initialisé, il affiche le tableau avec les fonds (cela veut dire que l'utilisateur n'a pas cliqué sur le bouton export funds)
-    */
-    /* if(!isset($_SESSION['export_funds']))
-    {
-        require 'pages/funds/funds_table.php';
-
-        //Il affiche le tableau si les utilisateurs ont le droit d'écriture sur le site
-        if($_SESSION['TequilaPHPWrite'] == "TequilaPHPWritetrue")
-        {
-            //Faire appel à la fonction qui fait et affiche le tableau, en ne passant rien comme id
-            funds_table($id_startup = "none");
-        }
-    }
-    else
-    {
-        //Supprimer la varaible de SESSION pour l'export des fonds
-        unset($_SESSION['export_funds']);
-    } 
- */
 }
 ?>
